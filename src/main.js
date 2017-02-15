08a5347caf7024bd164cdb5c5382c825c4bfa25c
sliwey <qlw1009@gmail.com>
2017-02-15 17:35:12 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 import './style/main.scss';
 import Vue from 'vue';
+import Vuex from 'vuex';
 import VueRouter from 'vue-router';
 import Layout from './pages/layout/layout';
 import Login from './pages/login/login';
 import Home from './pages/home/home';
 import User from './pages/user/user';
 
+Vue.use(Vuex);
 Vue.use(VueRouter);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    add (state) {
+      state.count++;
+    }
+  }
+});
 
 const router = new VueRouter({
   mode: 'history',
@@ -23,5 +36,6 @@ const router = new VueRouter({
 });
 
 const app = new Vue({
+  store,
   router
 }).$mount('#root');
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="home">
-    <p>I'm Home.</p>
+    <p>I'm Home.{{ count }}</p>
+    <button type="button" v-on:click="add">Add 1</button>
     <router-link to="/user">User</router-link>
   </div>
 </template>
 
 <script>
 
-
 export default {
   data () {
     return {
 
     }
+  },
+  computed: {
+    count () {
+      return this.$store.state.count;
+    }
+  },
+  methods: {
+    add () {
+      this.$store.commit('add');
+    }
   }
-
 }
 </script>
 
